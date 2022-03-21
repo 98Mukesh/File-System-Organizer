8e74e3c7f9237a74e5322ebe5a6f3fd2230d61b6
98Mukesh <mukesh121198@gmail.com>
2022-03-21 21:15:29 +0530

    .

--- Epic Games WebScraper/web2.js
+++ Epic Games WebScraper/web2.js
@@ -14,69 +14,49 @@ function getFinalUrl(url) {
     })
 }
 
-
 function extractData(html) {
     let $ = cheerio.load(html)
 
     let gameName = $('.css-spkfbn[data-component="PDPTitleHeader"]').text()
 
-    if (gameName.length > 32) {
+    if (gameName.length > 30) {
         let gameArr = gameName.split(':')
         gameName = gameArr[0]
     }
 
-    let info = $('.css-1g0mw3g[data-component="MarkdownParagraph"]')
-    let gameInfo = '-> Info :\n\n'
-    for (let i = 0; i < info.length; i++) {
-        gameInfo += `• ${$(info[i]).text()} \n\n`
+    let info = $('[data-component="MarkdownParagraph"]')
+    let gameInfo = 'Info :' + '\n' + $(info[0]).text() + '\n\n' + $(info[1]).text()
+
+
+    let genreFeature = $('.css-encdnt .css-1pj7rfu')
+    let genre = $(genreFeature[0]).text() + '\r' + $(genreFeature[1]).text()
+    let features = $(genreFeature[2]).text() + '\r' + $(genreFeature[3]).text()
+
+
+    let developerCompany = $('.css-b6wrti .css-1k1wbhe')
+    let dvlCom = ""
+    for (let i = 0; i < developerCompany.length; i++) {
+        dvlCom += $(developerCompany[i]).text() + '\n'
     }
 
 
-    let genreFeatureHeading = $('.css-encdnt .css-i96ixb')
-    let genreFeatureData = $('.css-encdnt .css-7agjck')
-    let genre = `-> ${$(genreFeatureHeading[0]).text()} : \n`
-    let features = `-> ${$(genreFeatureHeading[1]).text()} : \n`
+    let specs = 'Specifications :'
+    let minSpecs = 'Minimum Requirements :\n\n'
+    let recommendedSpecs = 'Recommended Requirements :\n\n'
 
-    for (let i = 0; i < genreFeatureData.length; i++) {
-        if (i < genreFeatureData.length / 2) {
-            genre += `•  ${$(genreFeatureData[i]).text()}  \n`
-        }
-        else {
-            features += `•  ${$(genreFeatureData[i]).text()}  \n`
-        }
-    }
-
-
-    let developerCompany1 = $('.css-b6wrti .css-i96ixb')
-    let developerCompany2 = $('.css-b6wrti .css-btns76')
-    let dvlCom = '-> Development And Release Date :\n\n'
-    for (let i = 0; i < developerCompany1.length; i++) {
-        if (i == developerCompany1.length) {
-            let window = $(developerCompany1[i]).text().split(/(?=[A-Z])/)
-            dvlCom += `• ${$(developerCompany1[i]).text()}  : ${window} } \n`
-        }
-        dvlCom += `• ${$(developerCompany1[i]).text()}  :  ${$(developerCompany2[i]).text()} \n`
-    }
-
-
-    let specs = '-> Specifications :'
-    let minSpecs = '• Minimum Requirements :\n\n'
-    let recommendedSpecs = '• Recommended Requirements :\n\n'
-
-    let spec1 = $('.css-3rds8q .css-i96ixb[data-component]')
-    let spec2 = $('.css-3rds8q .css-1yqcr93[data-component]')
-    for (let i = 0; i < spec1.length; i++) {
+    let spec = $('.css-3rds8q .css-2sc5lq')
+    for (let i = 0; i < spec.length; i++) {
         if (i % 2 == 0) {
-            minSpecs += `□ ${$(spec1[i]).text()} : ${$(spec2[i]).text()} \n`
+            minSpecs += $(spec[i]).text() + '\n'
         }
         else {
-            recommendedSpecs += `□ ${$(spec1[i]).text()} : ${$(spec2[i]).text()} \n`
+            recommendedSpecs += $(spec[i]).text() + '\n'
         }
     }
 
 
     let language = $('.css-k204li span')
-    let supportedLang = `-> Supported Languages :\n\n • ${$(language[0]).text()}\n\n • ${$(language[1]).text()}`
+    let supportedLang = 'Supported Languages : \r\r' + $(language[0]).text() + '\r\r' + $(language[1]).text()
 
 
     webObj.ps(gameName, gameInfo, genre, features, dvlCom, specs, minSpecs, recommendedSpecs, supportedLang)
